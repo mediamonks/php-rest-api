04731c4e4a1a2af7ab86c0a38f31d644fd282871
Robert <robert@mediamonks.com>
2017-02-01 12:59:32 +0100

    Separate how options are handled

--- src/EventSubscriber/RestApiEventSubscriberFactory.php
+++ src/EventSubscriber/RestApiEventSubscriberFactory.php
@@ -17,6 +17,24 @@ class RestApiEventSubscriberFactory
      */
     public static function create(array $options = [])
     {
+        $options = self::getOptions($options);
+        $requestTransformer = new RequestTransformer($options['serializer']);
+        $responseModelFactory = new ResponseModelFactory($options['response_model']);
+        $responseTransformer = new ResponseTransformer(
+            $options['serializer'],
+            $responseModelFactory,
+            self::getResponseTransformerOptions($options)
+        );
+
+        return new RestApiEventSubscriber($options['request_matcher'], $requestTransformer, $responseTransformer);
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    protected static function getOptions(array $options = [])
+    {
         if (empty($options['path'])) {
             $options['path'] = '/api';
         }
@@ -29,6 +47,16 @@ class RestApiEventSubscriberFactory
         if (empty($options['request_matcher'])) {
             $options['request_matcher'] =  new PathRequestMatcher($options['path']);
         }
+
+        return $options;
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    protected static function getResponseTransformerOptions(array $options = [])
+    {
         $responseTransformerOptions = [];
         if (isset($options['debug'])) {
             $responseTransformerOptions['debug'] = $options['debug'];
@@ -37,14 +65,6 @@ class RestApiEventSubscriberFactory
             $responseTransformerOptions['post_message_origin'] = $options['post_message_origin'];
         }
 
-        $requestTransformer = new RequestTransformer($options['serializer']);
-        $responseModelFactory = new ResponseModelFactory($options['response_model']);
-        $responseTransformer = new ResponseTransformer(
-            $options['serializer'],
-            $responseModelFactory,
-            $responseTransformerOptions
-        );
-
-        return new RestApiEventSubscriber($options['request_matcher'], $requestTransformer, $responseTransformer);
+        return $responseTransformerOptions;
     }
 }
